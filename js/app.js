a38068b1951c47ae19d334d971419ceeb43bcc91
Mark Swearingen <markswearingen@bendcable.com>
2019-09-30 21:11:39 -0700

    moved rendering to sales.html.  Added daily totals.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 'use strict';
+//TO DO LIST:
+//put sales.html into pages folder
+
 
 //defining global variables
 var hrOp = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']; 
@@ -13,20 +16,13 @@ var seattle = {
   avgCookHrLoc: [ ],
   totDayCook: 0,
   estCookHrLoc: function() {
-    //calculate the cookies per hour per location
-    //store value in avgCookHrLoc array
     for (var i = 0; i < hrOp.length; i++) {
       var estCookHr = Math.floor ((Math.random() * (this.maxCustHr - this.minCustHr) + this.minCustHr) * this.avgCookCust);
-      console.log(estCookHr);
-      console.log(hrOp[i]);
       this.avgCookHrLoc.push(estCookHr);
-      console.log(this.avgCookHrLoc);
     }
   },
   render: function(){
     var body = document.getElementById('body');
-    console.log(body);
-
     var h2 = document.createElement('h2');
     h2.textContent = this.location;
     body.appendChild(h2);
@@ -41,14 +37,14 @@ var seattle = {
       li.textContent = `${hrOp[i]} ${this.avgCookHrLoc[i]} cookies`;
       ul.appendChild(li);
 
-      var totDayCook = totDayCook + this.avgCookHrLoc[i]
+      var totDayCook = totDayCook + this.avgCookHrLoc[i];
       if (i === this.avgCookHrLoc.length - 1){
         li = document.createElement('li');
         li.textContent = `Total: ${totDayCook} cookies`;
         ul.appendChild(li);
       }
     }
-  },
+  }
 };
 
 var tokyo = {
@@ -57,14 +53,34 @@ var tokyo = {
   maxCustHr: 24,
   avgCookCust: 1.2,
   avgCookHrLoc: [ ],
+  totDayCook: 0,
   estCookHrLoc: function() {
-    //calculate the cookies per hour per location
-    //store value in avgCookHrLoc array
     for (var i = 0; i < hrOp.length; i++) {
       var estCookHr = Math.floor ((Math.random() * (this.maxCustHr - this.minCustHr) + this.minCustHr) * this.avgCookCust);
-      console.log(estCookHr);
-      console.log(hrOp[i]);
-      //console.log(location);
+      this.avgCookHrLoc.push(estCookHr);
+    }
+  },
+  render: function(){
+    var body = document.getElementById('body');
+    var h2 = document.createElement('h2');
+    h2.textContent = this.location;
+    body.appendChild(h2);
+
+    var ul = document.createElement('ul');
+    h2.appendChild(ul);
+
+    totDayCook = 0;
+    for(var i = 0; i < this.avgCookHrLoc.length; i++){
+      var li = document.createElement('li');
+      li.textContent = `${hrOp[i]} ${this.avgCookHrLoc[i]} cookies`;
+      ul.appendChild(li);
+
+      var totDayCook = totDayCook + this.avgCookHrLoc[i];
+      if (i === this.avgCookHrLoc.length - 1){
+        li = document.createElement('li');
+        li.textContent = `Total: ${totDayCook} cookies`;
+        ul.appendChild(li);
+      }
     }
   }
 };
@@ -76,13 +92,33 @@ var dubai = {
   avgCookCust: 3.7,
   avgCookHrLoc: [ ],
   estCookHrLoc: function() {
-    //calculate the cookies per hour per location
-    //store value in avgCookHrLoc array
     for (var i = 0; i < hrOp.length; i++) {
       var estCookHr = Math.floor ((Math.random() * (this.maxCustHr - this.minCustHr) + this.minCustHr) * this.avgCookCust);
-      console.log(estCookHr);
-      console.log(hrOp[i]);
-      //console.log(location);
+      this.avgCookHrLoc.push(estCookHr);
+    }
+  },
+  render: function(){
+    var body = document.getElementById('body');
+    var h2 = document.createElement('h2');
+    h2.textContent = this.location;
+    body.appendChild(h2);
+
+    var ul = document.createElement('ul');
+    h2.appendChild(ul);
+
+    totDayCook = 0;
+    
+    for(var i = 0; i < this.avgCookHrLoc.length; i++){
+      var li = document.createElement('li');
+      li.textContent = `${hrOp[i]} ${this.avgCookHrLoc[i]} cookies`;
+      ul.appendChild(li);
+
+      var totDayCook = totDayCook + this.avgCookHrLoc[i];
+      if (i === this.avgCookHrLoc.length - 1){
+        li = document.createElement('li');
+        li.textContent = `Total: ${totDayCook} cookies`;
+        ul.appendChild(li);
+      }
     }
   }
 };
@@ -94,13 +130,33 @@ var paris = {
   avgCookCust: 2.3,
   avgCookHrLoc: [ ],
   estCookHrLoc: function() {
-    //calculate the cookies per hour per location
-    //store value in avgCookHrLoc array
     for (var i = 0; i < hrOp.length; i++) {
       var estCookHr = Math.floor ((Math.random() * (this.maxCustHr - this.minCustHr) + this.minCustHr) * this.avgCookCust);
-      console.log(estCookHr);
-      console.log(hrOp[i]);
-      //console.log(location);
+      this.avgCookHrLoc.push(estCookHr);
+    }
+  },
+  render: function(){
+    var body = document.getElementById('body');
+    var h2 = document.createElement('h2');
+    h2.textContent = this.location;
+    body.appendChild(h2);
+
+    var ul = document.createElement('ul');
+    h2.appendChild(ul);
+
+    totDayCook = 0;
+    
+    for(var i = 0; i < this.avgCookHrLoc.length; i++){
+      var li = document.createElement('li');
+      li.textContent = `${hrOp[i]} ${this.avgCookHrLoc[i]} cookies`;
+      ul.appendChild(li);
+
+      var totDayCook = totDayCook + this.avgCookHrLoc[i];
+      if (i === this.avgCookHrLoc.length - 1){
+        li = document.createElement('li');
+        li.textContent = `Total: ${totDayCook} cookies`;
+        ul.appendChild(li);
+      }
     }
   }
 };
@@ -112,20 +168,46 @@ var lima = {
   avgCookCust: 4.6,
   avgCookHrLoc: [ ],
   estCookHrLoc: function() {
-    //calculate the cookies per hour per location
-    //store value in avgCookHrLoc array
     for (var i = 0; i < hrOp.length; i++) {
       var estCookHr = Math.floor ((Math.random() * (this.maxCustHr - this.minCustHr) + this.minCustHr) * this.avgCookCust);
-      console.log(estCookHr);
-      console.log(hrOp[i]);
-      //console.log(location);
+      this.avgCookHrLoc.push(estCookHr);
+    }
+  },
+  render: function(){
+    var body = document.getElementById('body');
+    var h2 = document.createElement('h2');
+    h2.textContent = this.location;
+    body.appendChild(h2);
+
+    var ul = document.createElement('ul');
+    h2.appendChild(ul);
+
+    totDayCook = 0;
+    
+    for(var i = 0; i < this.avgCookHrLoc.length; i++){
+      var li = document.createElement('li');
+      li.textContent = `${hrOp[i]} ${this.avgCookHrLoc[i]} cookies`;
+      ul.appendChild(li);
+
+      var totDayCook = totDayCook + this.avgCookHrLoc[i];
+      if (i === this.avgCookHrLoc.length - 1){
+        li = document.createElement('li');
+        li.textContent = `Total: ${totDayCook} cookies`;
+        ul.appendChild(li);
+      }
     }
   }
 };
 
+
+//Estimating number of cookies and rendering to DOM
 seattle.estCookHrLoc();
 seattle.render();
-// tokyo.estCookHrLoc();
-// dubai.estCookHrLoc();
-// paris.estCookHrLoc();
-// lima.estCookHrLoc();
+tokyo.estCookHrLoc();
+tokyo.render();
+dubai.estCookHrLoc();
+dubai.render();
+paris.estCookHrLoc();
+paris.render();
+lima.estCookHrLoc();
+lima.render();
