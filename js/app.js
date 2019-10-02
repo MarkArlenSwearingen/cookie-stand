3955d36c53b1149c705c51a413b66b6290339f2f
Mark Swearingen <markswearingen@bendcable.com>
2019-10-02 05:56:11 -0700

    Hourly and grand totals added.  Removed console logs.  Addressed linter issues

--- js/app.js
+++ js/app.js
@@ -3,12 +3,13 @@
 //put sales.html into pages folder
 //Make code more Dry by moving random into its own function.
 //Create single point of entry
-//Calculate new Daily Location Total(Done in lab 6) - Hour totals needed 10/2/2019
+//Place location objects data into single array.
 
 //defining global variables
 var hrOp = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var hourTot = [];
 var storeObjects = [];
+var grandTot = 0;
 
 //Create constructor functions;
 function Store(location, minCustHr, maxCustHr, avgCookCust, avgCookHrLoc, totDayCook) {
@@ -32,11 +33,7 @@ Store.prototype.render = function(){
   tr.appendChild(td);
   //write out location to global variable for accessing hourly totals;
   hourTot.push([this.location, []]);
-  storeObjects.push(this);//global variable for new design of summing hours;
-  // console.log(hourTot);
-  // console.log(storeObjects);
-  // console.log(storeObjects.length);
-  //add location to hourly total for all locations object
+  storeObjects.push(this);
 
   //loop through the array of cookies by hour to create DOM;
   for(var i = 0; i < this.avgCookHrLoc.length; i++){
@@ -46,9 +43,7 @@ Store.prototype.render = function(){
     var totDayCook = totDayCook + this.avgCookHrLoc[i];
 
     //write out cookies by hour to global variable for accessing hourly totals;
-    // console.log(hourTot.length-1);
     hourTot[hourTot.length-1][1].push(this.avgCookHrLoc[i]);
-    // console.log(hourTot);
   }
   //add totals by location to DOM;
   td = document.createElement('td');
@@ -88,48 +83,29 @@ Store.prototype.tableHead = function(){
 
 //add table footer to DOM
 Store.prototype.tableFooter = function(){
-  var body = document.getElementById('body');
-  // add Table to DOM;
-  var table = document.createElement('table');
-  // console.log(table);
-  table.setAttribute('id', 'daily');
-  body.appendChild(table);
+  var table = document.getElementById('daily');
   var tr = document.createElement('tr');
- 
   table.appendChild(tr);
-  // console.log(tr);
-  var th = document.createElement('th');
-  th.textContent = 'Totals';
-  tr.appendChild(th);
-  
-  //add table headers to DOM
+  var td = document.createElement('td');
+  td.textContent = 'Totals';
+  tr.appendChild(td);
+
   for (var i = 0; i < hrOp.length; i++){
-    // console.log('hello');
-    th = document.createElement('th');
+    td = document.createElement('td');
     //loop for hourly total
     var hourTotSum = 0;
-    // console.log(storeObjects.length);
     for(var j = 0; j < storeObjects.length; j++){
       var storeName = storeObjects[j];
-      console.log(storeName.location);
-
-      console.log(storeName.avgCookHrLoc[i]);
-      
-      // console.log(hourTot);
-      // if (hourTot[j][0] === this.location){
-      // console.log(hourTot[j][0]);
-      // console.log('hello');
-      // hourTotSum =+ hourTotSum;
-      // }
+      hourTotSum = hourTotSum + storeName.avgCookHrLoc[i];
     }
-    th.textContent = hourTotSum;//revise to totals from global variable hourTot;
-    tr.appendChild(th);
+    td.textContent = hourTotSum;
+    tr.appendChild(td);
+    grandTot = grandTot + hourTotSum;
   }
   //add Hourly Grand Total text for row header
-  th = document.createElement('th');
-  th.textContent = 'Grand Total goes here';
-  tr.appendChild(th);
-  // console.log(typeof(hourTot));
+  td = document.createElement('td');
+  td.textContent = `Grand Total: ${grandTot}`;
+  tr.appendChild(td);
 };
 
 var seattle = new Store('Seattle', 23, 65, 6.3, [], 0);
@@ -139,8 +115,7 @@ var paris = new Store('Paris', 20, 38, 2.3, [], 0);
 var lima = new Store('Lima', 2, 16, 4.6, [], 0);
 
 //Estimating number of cookies and rendering to DOM
-//TODO  place data above into single array.
-//      write a loop function to estimate the hourly cookie total
+
 seattle.tableHead();
 
 seattle.estCookHrLoc();
